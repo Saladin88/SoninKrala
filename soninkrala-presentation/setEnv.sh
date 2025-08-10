ed5d751fb9c96f7e5a564e69467d3f8416ead788
Saladin88 <145976634+Saladin88@users.noreply.github.com>
2025-08-10 03:52:22 +0200

    Update setEnv.sh

--- soninkrala-presentation/setEnv.sh
+++ soninkrala-presentation/setEnv.sh
@@ -1,7 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+: "${BACK_BASE_URL:?BACK_BASE_URL is not set}"
+
 echo "Start of the envs modification"
-soninkralaBackBaseUrl=${BACK_BASE_URL}
-escapedBackUrl=$(printf '%s\n' "$soninkralaBackBaseUrl" | sed -e 's/[\/&]/\\&/g')
 
-sed -i "s|backBaseUrl : .*|backBaseUrl : '${escapedBackUrl}'|" src/environments/environment.staging.ts
+# Échapper / et & pour sed (2 caractères problématiques dans la partie remplacement)
+escapedBackUrl=$(printf '%s' "$BACK_BASE_URL" | sed 's/[\/&]/\\&/g')
 
-echo "backBaseUrl updated with : $escapedBackUrl"
+# Remplace littéralement le token BACK_BASE_URL où qu'il soit
+sed -i "s|BACK_BASE_URL|$escapedBackUrl|g" src/environments/environment.staging.ts
+
+echo "Updated backBaseUrl to: $BACK_BASE_URL"
