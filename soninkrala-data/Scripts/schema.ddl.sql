301f36ed56595c18df70c88d73a33b81c5c7028e
Saladin88 <145976634+Saladin88@users.noreply.github.com>
2025-08-26 20:50:31 +0200

    Update schema.ddl.sql

--- soninkrala-data/Scripts/schema.ddl.sql
+++ soninkrala-data/Scripts/schema.ddl.sql
@@ -1,7 +1,17 @@
-DROP TABLE IF EXISTS t_questions, t_answers, t_accounts, t_roles, t_audios_letters, t_letters, t_photos, t_quiz, t_questions_t_quiz CASCADE ;
+DROP TABLE IF EXISTS t_questions, t_answers, t_accounts, t_roles, t_audios_letters, t_letters, t_photos, t_quiz, t_questions_t_quiz,t_pronunciation_attempts,t_term_versions,t_accounts_t_quiz_attempts CASCADE ;
+
+CREATE TABLE t_pronunciation_attempts (
+                        id INT GENERATED ALWAYS AS IDENTITY,
+                        pronunciation_word VARCHAR(50),
+                        attempted_at DATETIME,
+                        similarity_score DECIMAL(5,2) NOT NULL,
+                        CONSTRAINT t_pronunciation_attempts_pkey PRIMARY KEY(id),
+                        CONSTRAINT t_pronunciation_attempts_ukey UNIQUE (pronunciation_word,attempted_at)
+);
+
 
 CREATE TABLE t_photos (
-                          id int GENERATED ALWAYS AS IDENTITY,
+                          id INT GENERATED ALWAYS AS IDENTITY,
                           picture_name varchar(120) NOT NULL,
                           url_photo varchar(200) NOT NULL,
                           CONSTRAINT t_photos_pkey PRIMARY KEY (id),
@@ -57,6 +67,15 @@ CREATE TABLE t_roles (
                          CONSTRAINT t_role_name_ukey UNIQUE (name)
 );
 
+CREATE TABLE t_term_versions (
+                          id int GENERATED ALWAYS AS IDENTITY,
+                          version VARCHAR(20) NOT NULL,
+                          published_at DATETIME NOT NULL,
+                          label_version VARCHAR(100),
+                          CONSTRAINT t_term_versions_pkey PRIMARY KEY(id),
+                          CONSTRAINT t_term_versions_ukey UNIQUE (version)
+);
+
 CREATE TABLE t_accounts (
                             id INT GENERATED ALWAYS AS IDENTITY,
                             firstname varchar(50) NOT NULL,
@@ -66,15 +85,42 @@ CREATE TABLE t_accounts (
                             password varchar(80) NOT NULL,
                             profile_image varchar(200),
                             is_verify BOOLEAN NOT NULL,
+                            rgpd_accepted_at DATETIME NOT NULL,
                             creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                             uuid_token UUID,
                             uuid_token_expiration TIMESTAMP,
                             id_role INT NOT NULL,
+                            id_rgpd_version NOT NULL,
                             CONSTRAINT t_accounts_pkey PRIMARY KEY (id),
                             CONSTRAINT t_accounts_email_ukey UNIQUE (email),
                             CONSTRAINT t_accounts_username_ukey UNIQUE (username),
                             CONSTRAINT t_accounts_t_roles_fkey FOREIGN KEY (id_role)
-                                REFERENCES t_roles (id)
+                                REFERENCES t_roles (id),
+                            CONSTRAINT t_accounts_t_term_versions_fkey FOREIGN KEY (id_rgpd_version)
+                                REFERENCES t_term_versions (id)
+);
+
+CREATE TABLE t_accounts_t_quiz_attempts (
+                           id_account INT,
+                           id_quiz INT,
+                           score INT NOT NULL,
+                           attempt_number INT,
+                           completed_at DATETIME NOT NULL,
+                           CONSTRAINT t_accounts_t_quiz_attempts_pkey PRIMARY KEY(id_account, id_quiz),
+                           CONSTRAINT t_accounts_t_quiz_attempts_ukey UNIQUE(id_account, id_quiz,completed_at),
+                           CONSTRAINT t_accounts_t_quiz_attempts_t_accounts_fkey FOREIGN KEY(id_account) REFERENCES t_accounts(id),
+                           CONSTRAINT t_accounts_t_quiz_attempts_t_quiz_attempts_fkey FOREIGN KEY(id_quiz) REFERENCES t_quiz_attempts(id)
+);
+
+CREATE TABLE t_accounts_t_pronunciation_attempts (
+                        id_account INT,
+                        id_pronunciation INT,
+                        attempted_at DATETIME,
+                        similarity_score DECIMAL(5,2) NOT NULL,
+                        CONSTRAINT t_accounts_t_pronunciation_attempts_pkey PRIMARY KEY(id_account,id_pronunciation),
+                        CONSTRAINT t_accounts_t_pronunciation_attempts_ukey UNIQUE(id_account, id_pronunciation,attempted_at),
+                        CONSTRAINT t_accounts_t_quiz_attempts_t_accounts_fkey FOREIGN KEY(id_account) REFERENCES t_accounts(id),
+                        CONSTRAINT t_accounts_t_quiz_attempts_t_pronunciation_attempts_fkey FOREIGN KEY(id_pronunciation) REFERENCES t_pronunciation_attempts(id)
 );
 
 
